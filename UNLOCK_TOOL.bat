191620425ec54421dfa595350bdc132e9e2c787e
mrmazakblu <mrmazakblu@gmail.com>
2018-11-16 09:33:16 -0500

    corrected archive address

--- UNLOCK_TOOL.bat
+++ UNLOCK_TOOL.bat
@@ -37,7 +37,7 @@ echo start %~dp0UNLOCK_TOOL.bat >> %~dp0unlock-tool-update\unlock-tool-update.ba
 echo timeout 2 >> %~dp0unlock-tool-update\unlock-tool-update.bat
 echo exit >> %~dp0unlock-tool-update\unlock-tool-update.bat
 echo Downloading files from GitHub Repo
-files\wget.exe -P %~dp0unlock-tool-update\ https://github.com/mrmazakblu/bl-unlock-slock.git 2> update-logs\tool-download-log.txt
+files\wget.exe -P %~dp0unlock-tool-update\ https://github.com/mrmazakblu/bl-unlock-slock/archive/master.zip 2> update-logs\tool-download-log.txt
 %~dp0files\unzip.exe -u %~dp0unlock-tool-update\master.zip -d %~dp0unlock-tool-update\
 start %~dp0unlock-tool-update\unlock-tool-update.bat
 echo DONE WITH DOWNLOAD. EXITING NOW TO UPDATE THE FILES AND THIS SCRIPT
