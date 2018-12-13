05f8240e93998a3bb109af1e47bc9bb5bf218e29
mrmazakblu <mrmazakblu@gmail.com>
2018-12-13 12:24:11 -0500

    trimmed files no longer used

--- files/nvme-edit.bat
+++
@@ -1,7 +0,0 @@
-files\dd.exe if=files\b.txt of=modified-nvme seek=0x29C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0x49C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0x69C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0x89C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0xA9C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0xC9C10 bs=1 count=1
-files\dd.exe if=files\b.txt of=modified-nvme seek=0xE9C10 bs=1 count=1
